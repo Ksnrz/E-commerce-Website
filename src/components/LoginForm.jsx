4181e4d2848002e81620b85fabbf713d8e93329e
Ksnrz <kritsanaratz@gmail.com>
2024-11-10 19:12:11 +0700

    Login Form Almost finish wait for google login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,12 +2,33 @@ import React from "react";
 const LoginForm = () => {
   return (
     <>
-      <div className="p-[2.5rem]">
+      <div className="m-[2rem] max-h-screen">
         <div className="flex w-full">
-          <div className="flex flex-1 flex-col bg-red-300 justify-center items-center">
+          <div className="flex flex-1 flex-col justify-center items-center bg-slate-300">
             <div className="text-left">
               <h1 className="text-4xl mb-2.5">ยินดีต้อนรับ!</h1>
-              <p>โปรดเข้าสู่ระบบ และ เริ่มใช้งานได้เลย</p>
+              <p className="mb-8">โปรดเข้าสู่ระบบ และ เริ่มใช้งานได้เลย</p>
+              <p>Email</p>
+              <input
+                className="px-3 py-2.5 rounded-xl w-[350px] mb-4 border-2 input-border-color"
+                placeholder="Example@gmail.com"
+              ></input>
+              <p>Password</p>
+              <input
+                className="px-3 py-2.5 rounded-xl w-[350px] mb-4 border-2 input-border-color"
+                placeholder="Password"
+              ></input>
+              <div className="text-right">
+                <p className="mb-4 underline forgot-password-color">
+                  Forgot password?
+                </p>
+              </div>
+              <button className="bg-blue-900 w-full py-2.5 rounded-xl text-white mb-8">
+                Sign In
+              </button>
+              <div class="divider">
+                <span>or</span>
+              </div>
             </div>
           </div>
           <div className="flex-1">
