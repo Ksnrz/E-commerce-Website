9a0bc108bbdc01116218f452c24d481591696f24
Ksnrz <kritsanaratz@gmail.com>
2024-11-10 17:09:23 +0700

    added img for login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import MainProductPage from "./components/MainProductPage";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar />
+      <MainProductPage /> */}
+      <LoginForm />
     </>
   );
 }
