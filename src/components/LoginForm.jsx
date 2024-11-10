2dbb340952b6054ec9cd07ac523885dc620d6fc2
Ksnrz <kritsanaratz@gmail.com>
2024-11-10 19:28:33 +0700

    added thai language

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,36 +4,38 @@ const LoginForm = () => {
     <>
       <div className="m-[2rem] max-h-screen">
         <div className="flex w-full">
-          <div className="flex flex-1 flex-col justify-center items-center bg-slate-300">
+          <div className="flex flex-1 flex-col justify-center items-center">
             <div className="text-left">
               <h1 className="text-4xl mb-2.5">ยินดีต้อนรับ!</h1>
-              <p className="mb-8">โปรดเข้าสู่ระบบ และ เริ่มใช้งานได้เลย</p>
-              <p>Email</p>
+              <p className="mb-8 text-xl">
+                โปรดเข้าสู่ระบบ และ เริ่มใช้งานได้เลย
+              </p>
+              <p>อีเมล</p>
               <input
                 className="px-3 py-2.5 rounded-xl w-[350px] mb-4 border-2 input-border-color"
                 placeholder="Example@gmail.com"
               ></input>
-              <p>Password</p>
+              <p>รหัสผ่าน</p>
               <input
                 className="px-3 py-2.5 rounded-xl w-[350px] mb-4 border-2 input-border-color"
                 placeholder="Password"
               ></input>
               <div className="text-right">
                 <p className="mb-4 underline forgot-password-color">
-                  Forgot password?
+                  ลืมรหัสผ่าน?
                 </p>
               </div>
-              <button className="bg-blue-900 w-full py-2.5 rounded-xl text-white mb-8">
-                Sign In
+              <button className="bg-blue-900 w-full py-2.5 rounded-xl text-white mb-8 text-xl">
+                เข้าสู่ระบบ
               </button>
               <div class="divider">
                 <span>or</span>
               </div>
             </div>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 shadow-lg">
             <img
-              className="w-full max-h-screen rounded-[16px]"
+              className="w-full max-h-screen rounded-[16px] "
               src="/src/img/paint-login.png"
             />
           </div>
